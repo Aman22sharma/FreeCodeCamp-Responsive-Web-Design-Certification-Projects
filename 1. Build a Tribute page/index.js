af154999ce473ee2bdfe8e41bab9a0e8ae485078
Aman kumar sharma <kumaraman.rose@gmail.com>
2020-05-23 09:13:56 +0530

    Update index.js

--- 1. Build a Tribute page/index.js
+++ 1. Build a Tribute page/index.js
@@ -1,4 +1,4 @@
-// coded by @CAman kumar sharma 8 Aug 2019
+// coded by @Aman kumar sharma 8 Aug 2019
 
 const projectName = 'tribute-page';
 localStorage.setItem('example_project', 'Tribute Page');
